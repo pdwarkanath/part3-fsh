dec0b68ac13a1aee7bcb9b8d161f31df418f2ddf
pdwarkanath <dwarkanath.prabhu@gmail.com>
2022-05-15 17:30:31 +0530

    frontend build

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('req', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req'))
 
