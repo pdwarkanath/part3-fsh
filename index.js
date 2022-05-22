1ed0664184e252e164a9926d5d41a3dc3ff244f6
pdwarkanath <dwarkanath.prabhu@gmail.com>
2022-05-23 00:58:12 +0530

    cleared lint errors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-morgan.token('req', function (req, res) { return JSON.stringify(req.body) })
+morgan.token('req', req => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req'))
 
 
@@ -78,7 +78,7 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
-        return response.status(400).send({ error: error.message })
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
