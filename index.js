d4d98ebbc47fcbb52ca1c06db9e35aacf8bad304
pdwarkanath <dwarkanath.prabhu@gmail.com>
2022-05-23 00:48:58 +0530

    ex 3.19-22

--- index.js
+++ index.js
@@ -22,61 +22,49 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person=>{
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    }).catch(error => next(error))
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        }).catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
     Person.countDocuments({}, (err, count) => {
         const d = new Date()
         response.send(
-        `<p>Phonebook has info for ${count} people</p>
-        <p>${d.toString()}</p>`
+            `<p>Phonebook has info for ${count} people</p>
+            <p>${d.toString()}</p>`
         )
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    Person.findByIdAndRemove(request.params.id).then(result => {
-        response.status(204).end()
-    }).catch(error => next(error))
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(() => response.status(204).end())
+        .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
+    console.log(name, number)
+    Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
+        .then(updatedPerson => response.json(updatedPerson))
+        .catch(error => next(error))
+})
+
+app.post('/api/persons',(request, response, next) => {
     const body = request.body
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
-        .then(updatedPerson => response.json(updatedPerson))
-        .catch(error => next(error))    
-})
-
-app.post('/api/persons',(request, response) => {
-    const body = request.body
-    if (!body.name) {
-        return response.status(400).json({ 
-            error: 'name missing' 
-        })
-    }
-    if (!body.number) {
-        return response.status(400).json({ 
-            error: 'name missing' 
-        })
-    }
-
-    const person = new Person({
-        name: body.name, 
-        number: body.number
     })
-    person.save().then(savedPerson => response.json(savedPerson))
+    person.save()
+        .then(savedPerson => response.json(savedPerson))
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -87,9 +75,10 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-  
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message })
     }
     next(error)
 }
